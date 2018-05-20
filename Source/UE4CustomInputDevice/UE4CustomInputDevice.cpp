1303d8c0d2f3aadca34280f2e877b835ee9d62da
haisongliang <haisongliang@gmail.com>
2018-05-20 14:11:08 +0800

    Initial commit

---
+++ Source/UE4CustomInputDevice/UE4CustomInputDevice.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "UE4CustomInputDevice.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, UE4CustomInputDevice, "UE4CustomInputDevice" );
---
+++ Source/UE4CustomInputDevice/UE4CustomInputDeviceGameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "UE4CustomInputDeviceGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UE4CUSTOMINPUTDEVICE_API AUE4CustomInputDeviceGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
